52558867dd8d5c00c4afe40d4fcece03e11dd01f
SharkMouth <40568844+SharkMouth@users.noreply.github.com>
2024-11-07 12:36:23 +0700

    Update JavaScript Register

--- js/register.js
+++ js/register.js
@@ -1,20 +1,38 @@
 function submitRegister() {
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
-    const password = document.getElementById("newPass").value;
+    const newPassword = document.getElementById("newPass").value;
+    const confPassword = document.getElementById("confNewPass").value;
+
+    const numberPattern = /\d/;
+    const passwordPattern = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
     const userData = {
         name: name,
         email: email,
-        password: password
+        newPassword: newPassword,
+        confPassword: confPassword
     };
 
-    // Mengubah objek menjadi string JSON
-    const jsonData = JSON.stringify(userData);
+    // Test the input string for digits
+    if (numberPattern.test(name)) {
+        alert("Name Cannot Contain Numbers");
+    } else {
+        if (!passwordPattern.test(newPassword) && !passwordPattern.test(confPassword)){
+            alert("Password must be at least 8 characters long and contain letters, numbers, and special characters.");
+        }
+        if (newPassword != confPassword) {
+            alert("Passwords do not match");
+        } else {
+            // Mengubah objek menjadi string JSON
+            const jsonData = JSON.stringify(userData);
 
-    // Menampilkan data JSON (Anda bisa menyimpannya atau mengirimnya ke server)
-    console.log(jsonData);
+            // Menampilkan data JSON (Anda bisa menyimpannya atau mengirimnya ke server)
+            console.log(jsonData);
 
-    // Jika Anda ingin menyimpan ke local storage (contoh):
-    localStorage.setItem('userData', jsonData);
+            // Jika Anda ingin menyimpan ke local storage (contoh):
+            localStorage.setItem('userData', jsonData);
+        }
+
+    }
 }
